df4226a0b122f1aed2a4854cad118d31eaa4f692
kimyoungdong2 <kimy34d@gmail.com>
2019-01-20 21:39:06 +0900

    표준편차 0.02기준으로 수정

--- config.py
+++ config.py
@@ -45,16 +45,16 @@ ws["D1"],ws["D14"],ws["D27"],ws["D40"]="Jain Index","Jain Index","Jain Index","J
 ws["E1"],ws["E14"],ws["E27"],ws["E40"]="예정","예정","예정","예정"
 
 for i in range(2,12):
-    ws.cell(row=i,column=2).value=0.05*(i-1)
+    ws.cell(row=i,column=2).value=0.02*(i-1)
 for i in range(2,12):
-    ws.cell(row=i+13,column=2).value=0.05*(i-1)
+    ws.cell(row=i+13,column=2).value=0.02*(i-1)
 for i in range(2,12):
-    ws.cell(row=i+26,column=2).value=0.05*(i-1)
+    ws.cell(row=i+26,column=2).value=0.02*(i-1)
 for i in range(2,12):
-    ws.cell(row=i+39,column=2).value=0.05*(i-1)
+    ws.cell(row=i+39,column=2).value=0.02*(i-1)
 
 
-in_advance_List=[0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5]
+in_advance_List=[0.02,0.04,0.06,0.08,0.10,0.12,0.14,0.16,0.18,0.20]
             
 The_end=0
 All_Fair=0
